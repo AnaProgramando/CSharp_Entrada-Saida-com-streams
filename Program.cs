08c543b615744ace3817f5f2bbd54c44a9e4601e
Ana Programando <anabe.valentim@gmail.com>
2022-01-21 10:27:06 -0300

    Usado método Close e using

--- Program.cs
+++ Program.cs
@@ -13,15 +13,16 @@ namespace CSharp_Entrada_Saida_com_streams
         {
             var enderecoDoArquivo = "contas.txt";
 
-            var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
-
-            var buffer = new byte[1024]; // 1 Kbyte
-            var numeroDeBytesLidos = -1;
-
-            while(numeroDeBytesLidos != 0)
+            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
             {
-                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-                EscreverBuffer(buffer);
+                var buffer = new byte[1024]; // 1 Kbyte
+                var numeroDeBytesLidos = -1;
+
+                while (numeroDeBytesLidos != 0)
+                {
+                    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                    EscreverBuffer(buffer);
+                }
             }
             
             Console.ReadLine();
