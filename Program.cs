c25934fa59be72e97c5d7fd77cbe13a8b622b598
Ana Programando <anabe.valentim@gmail.com>
2022-01-21 11:56:02 -0300

    Conversão do texto para contacorrente

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO; // IO: Input e Output, Entrada e Saída
+using CSharp_Entrada_Saida_com_streams.Modelos;
 
 namespace CSharp_Entrada_Saida_com_streams
 {
@@ -22,12 +23,41 @@ namespace CSharp_Entrada_Saida_com_streams
                     while (!leitor.EndOfStream)
                     {
                         var linha = leitor.ReadLine();
-                        Console.WriteLine(linha);
+                        var contaCorrente = ConverterStringParaContaCorrente(linha);
+
+                    var mensagem = $"Conta número {contaCorrente.Numero}, agência {contaCorrente.Agencia}. Saldo: R$ {contaCorrente.Saldo}";
+                    Console.WriteLine(mensagem);    
+
+                    // Console.WriteLine(linha);
                     }
                 }
 
             Console.ReadLine();
         }
+        
 
+        static ContaCorrente ConverterStringParaContaCorrente(string linha)
+        {
+            string[] campos = linha.Split(' ');
+
+            var agencia = campos[0];
+            var numero = campos[1];
+            var saldo = campos[2].Replace('.', ','); // Trocando o ponto do saldo pela vírgula
+            var nomeTitular = campos[3];
+
+            var agenciaComoInt = int.Parse(agencia);
+            var numeroComoInt = int.Parse(numero);
+
+            var saldoComoDouble = double.Parse(saldo);
+
+            var titular = new Cliente();
+            titular.Nome = nomeTitular;
+
+            var resultado = new ContaCorrente(agenciaComoInt, numeroComoInt);
+            resultado.Depositar(saldoComoDouble);
+            resultado.Titular = titular;
+
+            return resultado;
+        }
     }
 }
