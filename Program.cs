3e614035abd8bcc23de50a631fefa9f64c038ca7
Ana Programando <anabe.valentim@gmail.com>
2022-01-21 11:24:54 -0300

    Usado o StreamReader

--- Program.cs
+++ Program.cs
@@ -7,39 +7,27 @@ using System.IO; // IO: Input e Output, Entrada e Saída
 
 namespace CSharp_Entrada_Saida_com_streams
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
             var enderecoDoArquivo = "contas.txt";
 
-            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+            using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
             {
-                var buffer = new byte[1024]; // 1 Kbyte
-                var numeroDeBytesLidos = -1;
+                var leitor = new StreamReader(fluxoDeArquivo);
 
-                while (numeroDeBytesLidos != 0)
+                // var linha = leitor.Read();                
+                // var linha = leitor.ReadToEnd();
+                
+                while(!leitor.EndOfStream)
                 {
-                    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-                    EscreverBuffer(buffer, numeroDeBytesLidos);
-                }
+                    var linha = leitor.ReadLine();
+                    Console.WriteLine(linha);
+                }                
             }
-            
-            Console.ReadLine();
-        }
 
-        static void EscreverBuffer(byte[] buffer, int bytesLidos)
-        {
-            var utf8 = Encoding.Default;
-
-            var texto = utf8.GetString(buffer, 0, bytesLidos);
-            Console.Write(texto);
-
-            //foreach (var meuByte in buffer)
-            //{
-            //    Console.Write(meuByte);
-            //    Console.Write(" ");
-            //}
+                Console.ReadLine();
         }
 
     }
